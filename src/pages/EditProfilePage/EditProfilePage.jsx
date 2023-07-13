6c2b7b728695645de56da2876a52536b2197502a
Ayoze Martin <ayozemt@gmail.com>
2023-07-13 21:33:28 +0200

    Create edit profile page

---
+++ src/pages/EditProfilePage/EditProfilePage.jsx
@@ -0,0 +1,82 @@
+import "./EditProfilePage.css";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import authService from "../../services/auth.service";
+
+function EditProfilePage() {
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [errorMessage, setErrorMessage] = useState(undefined);
+  const [profilePic, setProfilePic] = useState("");
+  const { userId } = useParams();
+
+  const navigate = useNavigate();
+
+  const handleName = (e) => setName(e.target.value);
+  const handleSurname = (e) => setSurname(e.target.value);
+  const handleProfilePic = (e) => setProfilePic(e.target.value);
+
+  const handleEditProfileSubmit = (e) => {
+    e.preventDefault();
+    const requestBody = { name, surname, profilePic };
+
+    authService
+      .edit(userId, requestBody)
+      .then((response) => {
+        console.log("Profile edited", response);
+      })
+      .then(() => {
+        navigate(`/profile`);
+      })
+      .catch((error) => {
+        const errorDescription = error.response.data.message;
+        setErrorMessage(errorDescription);
+      });
+  };
+
+  return (
+    <div className="EditProfilePage">
+      <h1>Editar perfil</h1>
+      <form onSubmit={handleEditProfileSubmit}>
+        <label className="form-label">Nombre</label>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleName}
+          className="form-control"
+          placeholder={name}
+        />
+        <label className="form-label">Apellido</label>
+        <input
+          type="text"
+          name="surname"
+          value={surname}
+          onChange={handleSurname}
+          className="form-control"
+          placeholder={surname}
+        />
+        <label className="form-label">Foto de perfil</label>
+        <input
+          type="text"
+          name="profilePic"
+          value={profilePic}
+          onChange={handleProfilePic}
+          className="form-control"
+          placeholder={profilePic}
+        />
+        <button type="submit" className="btn btn-dark">
+          Guardar cambios
+        </button>
+      </form>
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
+      <Link to={`/profile`} className="btn btn-dark">
+        Volver
+      </Link>
+    </div>
+  );
+}
+
+export default EditProfilePage;
