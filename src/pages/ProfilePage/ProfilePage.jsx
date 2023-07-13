bf9568f9b674a116d8931cdc6459e440e7860011
Ayoze Martin <ayozemt@gmail.com>
2023-07-13 14:50:23 +0200

    Add info and links to profile

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,10 +1,79 @@
 import "./ProfilePage.css";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../context/auth.context";
+import authService from "../../services/auth.service";
 
 function ProfilePage() {
+  const { user } = useContext(AuthContext);
+  const [profile, setProfile] = useState([]);
+
+  useEffect(() => {
+    const getProfile = () => {
+      authService
+        .profile(user._id)
+        .then((response) => setProfile(response.data))
+        .catch((error) => console.log(error));
+    };
+    getProfile();
+  }, [user._id]);
+
   return (
-    <div>
-      <h1>Profile page</h1>
-    </div>
+    user &&
+    profile && (
+      <div className="align-left">
+        <div>
+          <div className="profileHeader">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/5348/5348883.png"
+              alt="Profile"
+              className="profilePic"
+            />
+            <h1>Hola, {user.name}</h1>
+          </div>
+          <h2>Información personal</h2>
+          <p><b>Nombre:</b> {profile.name}</p>
+          <p><b>Apellido:</b> {profile.surname}</p>
+          <p><b>Email:</b> {user.email}</p>
+          <p><b>DNI:</b> {profile.dni}</p>
+        </div>
+        <hr />
+        <div>
+          <h2>Tus familias</h2>
+          {profile.family &&
+            profile.family.map((fam) => (
+              <div key={fam._id}>
+                <h3>
+                  <Link to={`/family/${fam._id}`}>{fam.surname}</Link>
+                </h3>
+                <p>Creada el: {fam.createdAt.split("T")[0]}</p>
+                <hr />
+              </div>
+            ))}
+        </div>
+        <div>
+          <h2>Cuenta</h2>
+          <Link
+            to={`/profile/${user._id}/edit`}
+            role="button"
+            className="btn btn-dark"
+            style={{ marginLeft: "30px", marginBottom: "10px" }}
+          >
+            Editar perfil
+          </Link>
+          <br />
+          <Link
+            to={`/profile/${user._id}/delete`}
+            role="button"
+            className="btn btn-danger"
+            style={{ marginLeft: "30px", marginBottom: "10px" }}
+          >
+            Eliminar cuenta
+          </Link>
+        </div>
+      </div>
+    )
   );
 }
 
