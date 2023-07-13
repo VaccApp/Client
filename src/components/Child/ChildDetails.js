2b6e1065d5c46b834ba388ea5dcc796267fe459d
Jon <jon_ander_23@protonmail.com>
2023-07-13 17:25:37 +0200

    Child details component renders all vaccines from a Child

--- src/components/Child/ChildDetails.js
+++ src/components/Child/ChildDetails.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import { Link, useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import childService from "../../services/child.service";
+import VaccineCard from "../Vaccine/VaccineCard";
 
 const API_URL = "http://localhost:5005";
 
@@ -16,22 +17,39 @@ export default function ChildDetails(props) {
       .catch((error) => console.log(error));
   };
 
+  function getAge() {
+    let hoy = new Date();
+    let cumple = new Date(child.birthdate);
+    let edad = hoy.getFullYear() - cumple.getFullYear();
+    var m = hoy.getMonth() - cumple.getMonth();
+
+    if (m < 0 || (m === 0 && hoy.getDate() < cumple.getDate())) {
+      edad--;
+    }
+    return edad;
+  }
+
+  function getMonths() {
+    let meses = getAge() * 12;
+    return meses;
+  }
+
   useEffect(() => {
     getAChild();
   }, []);
 
-  console.log("Child details", child);
+  console.log("Child details", child.vaccines);
   return (
     child && (
       <div>
-        <h1>Detalles de la familia {child.surname}:</h1>
-        <h2>Padres:</h2>
-        {/* {child.parents.map((parent) => (
-          <div key={parent._id} {...parent}>
-            <p>Name: {parent.name}</p>
-            <p>Email: {parent.email}</p>
-          </div>
-        ))} */}
+        <h1>Información de {child.name}:</h1>
+        <h2>Fecha de nacimiento: {child.birthdate.slice(0, 10)}</h2>
+        <h2>Edad: {getAge()} años</h2>
+        <h3>Vacunas: </h3>
+        {child.vaccines.map((vaccine) => {
+          return <VaccineCard key={vaccine._id} {...vaccine} />;
+        })}
+
         <Link to={`/child/${child._id}/children`}>
           <h2>Hijos:</h2>
         </Link>
