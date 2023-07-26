0e66d41ea7cc67eaf0d5d8af769eb026a9ce2cf6
Jon <jon_ander_23@protonmail.com>
2023-07-26 09:22:35 +0200

    VaccinePage info

--- src/pages/VaccinesPage/VaccinePage.js
+++ src/pages/VaccinesPage/VaccinePage.js
@@ -10,7 +10,7 @@ export default function VaccinePage(props) {
   const [vaccine, setVaccine] = useState([]);
   const { vaccineId } = useParams();
 
-  console.log("vaccineId", typeof vaccineId);
+  console.log("vaccineId front", typeof vaccineId);
 
   const getOneVaccine = (vaccineId) => {
     vaccineApiService
